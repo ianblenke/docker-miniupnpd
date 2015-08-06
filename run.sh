2b944b398e5fd6f4752f349e187f9192bf68e776
Ian Blenke <ian@blenke.com>
2015-08-06 15:17:42 -0400

    Identify external IP

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 EXTIFACE=${EXTIFACE:-eth0}
-INTIFACE=${INTIFACE:-docker0}
+INTIFACE=${INTIFACE:-172.17.42.1}
 INTNET=${INTNET:-172.17.0.0/16}
 
 grep -e '^START_DAEMON=' /etc/default/miniupnpd || \
@@ -19,18 +19,19 @@ sed -i -e "s/^iniUPnPd_ip6tables_enable=.*\$/iniUPnPd_ip6tables_enable=yes/" \
        -e "s/^START_DAEMON=.*\$/START_DAEMON=1/" \
        /etc/default/miniupnpd
 
-sed -i -e "s/^ext_ifname=.*\$/ext_ifname=${EXTIFACE}/" \
-       -e "s/^listening_ip=.*\$/listening_ip=${INTIFACE}/" \
-       -e "s/^enable_natpmp=.*\$/enable_natpmp=yes/" \
-       -e "s/^enable_upnp=.*\$/enable_upnp=yes/" \
-       -e "s%192\.168\.1\.0/24%${INTNET}%" \
-       /etc/miniupnpd/miniupnpd.conf
-
 . /etc/default/miniupnpd
 
 IP="/bin/ip"
 
 EXTIP="$(LC_ALL=C ${IP} addr show ${MiniUPnPd_EXTERNAL_INTERFACE} | grep "inet " | awk '{ print $2 }' | cut -d"/" -f1)"
+
+sed -i -e "s/^ext_ifname=.*\$/ext_ifname=${EXTIFACE}/" \
+       -e "s/^listening_ip=.*\$/listening_ip=${INTIFACE}/" \
+       -e "s/^enable_natpmp=.*\$/enable_natpmp=yes/" \
+       -e "s/^enable_upnp=.*\$/enable_upnp=yes/" \
+       -e "s%192.168.0.0/24%${$IP}/32%" \
+       -e "s%192\.168\.1\.0/24%${INTNET}%" \
+       /etc/miniupnpd/miniupnpd.conf
 
 # This is needed to keep running in the foreground in debug mode
 exec /usr/sbin/miniupnpd -i ${EXTIFACE} -o ${EXTIP} -a ${INTIFACE} -d
